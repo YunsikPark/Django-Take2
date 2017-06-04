6a4e89b3d9af29a34b4bbde8e41fde685ed1810c
Yunsik Park <space214@gmail.com>
2017-06-05 00:14:30 +0900

    modified models

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    question_text = models.CharField('질문내용', max_length=200)
+    pub_date = models.DateTimeField('발행일자')
 
     def __str__(self):
         return self.question_text
@@ -14,11 +14,19 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = '최근 발행 여부'
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(
+        Question,
+        verbose_name='해당질문',
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField('선택내용', max_length=200)
+    votes = models.IntegerField('총 투표수', default=0)
 
     def __str__(self):
         return self.choice_text
